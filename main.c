b4f4f8036a85ea24715524ae926573dd5f5fcd23
Cosmoooow <33749958+Cosmoooow@users.noreply.github.com>
2017-11-17 13:00:42 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,40 @@
 #include <stdio.h>
 
+deftype struct{
+	int type;
+	int team;
+} coord;
+
+
+
+void affichagecase(int team, int type,char * tabaff){
+	printf("%c\n",tabaff[team][type]);
+
+}
+
+int affichageplateau(char * tabaff);
+	for (int i=0; i>10; i++){
+		for (int j=0; j>10; j++){
+			type= plateau[i][j]->type;
+			team= plateau[i][j]->team;
+			affichecase(team,type,tabaff) ;
+		}
+	}
+}
+
+  
+int main(){
+char tabaff[3][2];
+tabaff[0][1]='▒';
+tabaff[1][1]='◇';
+tabaff[1][2]='△';
+tabaff[1][3]='○';
+tabaff[2][1]='◆';
+tabaff[2][2]='▲';
+tabaff[2][3]='●';
+	
+
+  
 int main(){
 
 
